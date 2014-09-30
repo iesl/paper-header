8161e5ece6c8f842d0dc2afee2a65ce0527073c6
k8si <ksilvers@umass.edu>
2014-09-29 21:15:41 -0400

    added Features object with some helper functions

---
+++ src/main/scala/edu/umass/cs/iesl/paperheader/Features.scala
@@ -0,0 +1,148 @@
+package edu.umass.cs.iesl.paperheader
+
+import java.util.regex.Pattern
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Created by kate on 9/29/14.
+ */
+
+// TODO maybe this proj should just depend on rexa1-metatagger so I can import ... ?
+
+object Features {
+  case class re(name:String, pattern:String) {
+    val p = Pattern.compile(pattern)
+    def apply(w:String) = p.matcher(w).matches
+    //def contains(w:String) = p.matcher(w).find
+  }
+  val wordClasses = List(
+    re("Year", RexaRegex.Year),
+    re("Url", RexaRegex.Url),
+    re("NameWithParticle", RexaRegex.NameWithParticle),
+    re("TechReport", RexaRegex.TechReport),
+    re("Phone", RexaRegex.Phone),
+    re("PossiblePages", RexaRegex.PossiblePages),
+    re("PageWord", RexaRegex.PageWord),
+    re("PossibleVol", RexaRegex.PossibleVol),
+    re("YearInParens", RexaRegex.YearInParens),
+    re("Numeric", RexaRegex.Numeric),
+    re("Digits", "[0-9]+"),
+    re("DigitsAndSeparators", "[0-9]+[\\-/][0-9\\-/]*[0-9]"),
+    re("DoTW", RexaRegex.DoTW),
+    re("Month", RexaRegex.Month),
+    re("CardinalDirection",RexaRegex.CardinalDirection),
+    re("Email", RexaRegex.Email),
+    re("BracketedNumber", RexaRegex.BracketedNumber),
+    re("Ordinal", RexaRegex.Ordinal),
+    re("OrdinalWritten", RexaRegex.OrdinalWritten),
+    re("EtAl", RexaRegex.EtAl),
+    re("Volume", "(?ii)Vol(?:ume|\\.?)"),
+    re("No", "(?ii)N(umber|o\\.?)"),
+    re("Initial", RexaRegex.Initial),
+    re("Editor", RexaRegex.Editor)
+  )
+
+  def simplify(w:String): String = {
+    for (m <- wordClasses; if m(w)) return m.name
+    return w.replaceAll("[0-9]","#").toLowerCase
+  }
+
+  def digitFeatures(w:String) = {
+    val f = new ArrayBuffer[String]
+    if (w matches "[0-9]+") {
+      f += "all-digits"
+      f += w.length + "-digit"
+      if (w.length != 4) f += "not-4-digit"
+    } else if (w matches ".*[0-9].*") {
+      f += "contains-digits"
+    } else {
+      f += "no-digits"
+    }
+    f
+  }
+
+
+}
+
+object RexaRegex {
+  val YearInParens = "\\(\\s*[0-9][0-9][0-9][0-9][a-z]?\\s*\\)"      // e.g. "(1994)"
+  val PossibleVol = "[0-9][0-9]?\\s*\\(\\s*[0-9]+\\s*\\)(?:\\s*:)?"  // e.g. "7(4)"
+  val PossiblePages = "[0-9]+\\s*-[\\s\\-]*[0-9]+"                   // e.g. "10 - 25" possible page numbers
+  val PageWord = "(?:\\b(?:(?:(?:pp|[Pp]gs?|p)(?:\\s*\\.|\\b))|[Pp]ages?))"
+  val MonthStrict = "(?:January|February|March|April|May|June|July|August|September|October|November|December)"
+  val Month = ("(?:(?ii)"
+    + "\\b(?:Jan(?:uary)?|Febr?(?:uary)?|Mar(?:ch)?|Apr(?:il)?"
+    + "|May|June?|July?|Aug(?:ust)?|Sept?(?:ember)?|Oct(?:ober)?|Nov(?:ember)?"
+    + "|Dec(?:ember)?)(?:\\s*[.,/\\-]+)?\\s*"
+    + ")+")
+  val Editor = "(?:(?:\\(\\s*)?(?:[Ee]ditors?|[Ee]ds?\\.)(:?\\s*\\))?|\\(\\s*eds?\\s*\\)?|edited\\s*by)"
+  val BracketedNumber = "\\[\\s*[0-9]+[a-z]*\\s*\\]"      // bracketed number and possibly a letter, e.g. "[15a]"
+  val Url = ("(?:(?:(?:https?|ftp|ssh)://.*|(?:www|ftp)\\.\\S+|[^()@]*\\.edu\\S*)"
+      + "|" + "\\((?:(?:https?|ftp|ssh)://.*|(?:www|ftp)\\.\\S+|[^()@]*\\.edu\\S*)\\))")
+  val Email = "\\S+@\\S+"
+  val Year = "(19|20)[0-9][0-9]"
+  val Numeric = "[0-9][0-9,]+\\.?[0-9]*\\b"
+  val DoTW = "(?:Mon|Tues?|Wed(?:nes)?|Thurs?|Fri|Satu?r?|Sun)(?:day)"
+  val CardinalDirection = "(?:(?ii)\\b(?:north|south|east|west)\\s*?(?:east|west|))\\b"
+  val Ordinal = "[0-9]+[\\-\\s]*(?:st|nd|rd|th)\\b"
+  val OrdinalWritten = ("(?:(?ii)"
+    + "first|second|third|fourth|fifth|sixth|seventh|eighth|ninth|tenth"
+    + "|eleventh|twelfth|thirteenth|fourteenth|fifteenth|sixteenth"
+    + "|seventeenth|eighteenth|nineteenth"
+    + "|twentieth|thirtieth|fou?rtieth|fiftieth|sixtieth|seventieth"
+    + "|eightieth|ninetieth|twentieth"
+    + "|hundredth|thousandth|millionth|billionth"
+    + ")")
+
+  val RefMarker = "^(?:\\[.*?\\]|[0-9]+\\s*\\.?)"
+
+  val _NameParticle = "(?:(?ii)de|da|della|mc|del|van|van\\s*der|von|de\\s+la|la|di|van|der|O\\s*'\\s*)"
+  val NameParticle = "\\b" + _NameParticle + "\\b"
+  val NameWithParticle = "\\b" + _NameParticle + "\\s*[A-Z][a-zA-Z]+"
+
+  val EtAl = "\\bet\\s*\\.?\\s*al\\b\\s*\\."
+  val Phone = "\\(?[0-9][0-9][0-9][\\s\\-\\)]+[0-9][0-9][0-9][\\s\\-]+[0-9][0-9][0-9][0-9]\\b"
+  val Alpha = "[A-ZÁÉÍÓÚÀÈÌÒÙÇÑÏÜa-zàèìòùáéíóúçñïü]"
+  val NewLine = "(?:-NEWLINE-|\\+L\\+)"
+  val Punc = "[,\\.;:?!()]"
+  val Caps = "[A-ZÁÉÍÓÚÀÈÌÒÙÇÑÏÜ]"
+  val CapitalizedAlphaWord = Caps + Alpha + "+"
+  val Acronym = "[A-Z][A-Z\\.]*\\.[A-Z\\.]*"
+  val InParens = "\\(.*\\)"
+
+  val TransitionPunc = "[\\.\"',]"
+  val ContainsPunc = ".*[\\.\"',].*"
+
+  val Initial = "(?:" + Caps + "\\." + "\\s*)+"
+  val SingleChar = Alpha
+  val SingleCapital = "[A-Z]"
+  val AllCaps = Caps + "+"
+
+  val TechReport = "[A-Z\\-/]+[/-]+[0-9\\-/]+"   // not exactly a techreport .. grabs "UAI-09" "AAAI-2003"
+
+  val References = (".*?(References?|REFERENCES?|Bibliography|BIBLIOGRAPHY"
+    + "|References?\\s+and\\s+Notes?|References?\\s+Cited"
+    + "|L(?i:ITERATURE CITED)"
+    + "|REFERENCES?\\s+CITED|REFERENCES?\\s+AND\\s+NOTES?):?\\s*$")
+  val Introduction = "[^A-Za-z]*I(?i:ntroduction)"
+  val Abstract = "[^A-Za-z]*A(?i:bstract)"
+  val Acknowledgments = "(?:(?ii)[^A-Za-z]*(Acknowledge?ments?))"
+  val CityStateZip = "\\b(?:[A-Z][a-z]+\\s*)+, [A-Z][A-Z] [0-9][0-9][0-9][0-9][0-9](?:-[0-9]+)?"
+  val WeakerAbstract = ".*\\bA(?:(?ii)bstract)\\s*:?$"
+  val Keywords = "^\\s*Keywords\\s*:.*"
+
+  // words which can appear in a name segment such as editors or authors
+  val validLookingButNotValidNameWords = "\\b(?:(?ii)in|thesis|the)\\b"
+  val ValidNameWord = List(
+    Initial,
+    SingleCapital,
+    CapitalizedAlphaWord,
+    NameWithParticle,
+    NameParticle,
+    EtAl,
+    ",",
+    "-+",
+    "and|&"
+  ).mkString("|")
+}
--- src/main/scala/edu/umass/cs/iesl/paperheader/HeaderCRF.scala
+++ src/main/scala/edu/umass/cs/iesl/paperheader/HeaderCRF.scala
@@ -4,10 +4,13 @@ import cc.factorie._
 import cc.factorie.app.nlp._
 import cc.factorie.variable._
 import cc.factorie.app.chain._
+import Features._
 
 /**
-* Created by kate on 9/25/14.
-*/
+ * Created by kate on 9/25/14.
+ */
+
+// TODO labels should maybe be on doc Sections, not Tokens ?
 
 class HeaderTagger extends DocumentAnnotator {
   /* DocumentAnnotator methods */
@@ -16,7 +19,13 @@ class HeaderTagger extends DocumentAnnotator {
   def postAttrs: Iterable[Class[_]] = List(classOf[HeaderField])
   def process(document:Document): Document = {
     if (document.tokenCount == 0) return document
-    //TODO stuff
+    val alreadyHadFeatures = document.hasAnnotation(classOf[FeatureVariable])
+    if (!alreadyHadFeatures) addFeatures(document)
+    for (token <- document.tokens) if (token.attr[HeaderField] eq null) token.attr += new HeaderField(token, "O")
+    val labels = document.tokens.map(_.attr[HeaderField]).toSeq
+    model.maximize(labels)(null)
+    if (!alreadyHadFeatures) { document.annotators.remove(classOf[FeatureVariable]); for (token <- document.tokens) token.attr.remove[FeatureVariable] }
+    //TODO document.attr.+=(new HeaderSectionBuffer ...) ?
     document
   }
 
@@ -29,12 +38,69 @@ class HeaderTagger extends DocumentAnnotator {
   class Model extends ChainModel[HeaderField, FeatureVariable, Token](HeaderFieldDomain, FeatureDomain, l => l.token.attr[FeatureVariable], l => l.token, t => t.attr[HeaderField])
   val model = new Model
   val objective = new HammingTemplate[LabeledHeaderField]
+
+
+
+  def addFeatures(doc:Document): Unit = {
+    import cc.factorie.app.strings.stringShape
+    doc.annotators(classOf[FeatureVariable]) = HeaderTagger.this.getClass
+    for (token <- doc.tokens) {
+      val feats = new FeatureVariable(token)
+      token.attr += feats
+      val word = token.string.replace(" ", "") // remove spaces
+      if (word matches RexaRegex.NewLine) { feats += "NEWLINE"; return; }
+      feats += s"SIMPLIFIED=${simplify(word)}"
+      feats ++= digitFeatures(word)
+      feats += s"SHAPE2=${stringShape(word, 2)}"
+      feats += s"SHAPE3=${stringShape(word, 1)}"
+
+      //TODO :
+      /*
+      for (m <- regexMatchers; if m(word)) this += m.name
+      lexicons.foreach(_(this))
+      nameLexicons.foreach(_(this))
+
+      def cmp(x:Int, y:Int) = {
+        if (x == y) "equal"
+        else if (x > y) "greater-than"
+        else "less-than"
+      }
+
+      if (hasPrev) {
+        this += "cmp(x)=" + cmp(x, prev.x)
+        this += "cmp(y)=" + cmp(y, prev.y)
+        if (font != prev.font) this += "change-in-font"
+      }
+       */
+    }
+
+
+  }
+
+  def train(trainDocs:Seq[Document], testDocs:Seq[Document], l1:Double=0.1, l2:Double=0.1, lr:Double=0.1)(implicit random:scala.util.Random): Double = {
+    0.0
+  }
+
+  //TODO serialize, deserialize
+}
+
+//TODO object HeaderTagger extends HeaderTagger([classpathURL] ...) etc.
+
+class HeaderTaggerOpts extends cc.factorie.util.CmdOptions with SharedNLPCmdOptions {
+  val saveModel = new CmdOption("save-model", "NER1.factorie", "STRING", "Filename for the model (saving a trained model or reading a running model.")
+  val serialize = new CmdOption("serialize", false, "BOOLEAN", "Whether to serialize at all")
+  val train = new CmdOption("train", "", "STRING", "Filename(s) from which to read training data")
+  val test = new CmdOption("test", "", "STRING", "Filename(s) from which to read test data")
+  val l1 = new CmdOption("l1", 0.02, "FLOAT", "L1 regularizer for AdaGradRDA training.")
+  val l2 = new CmdOption("l2", 0.000001, "FLOAT", "L2 regularizer for AdaGradRDA training.")
+  val learningRate = new CmdOption("learning-rate", 1.0, "FLOAT", "L2 regularizer for AdaGradRDA training.")
+}
+
+object HeaderTaggerTrainer extends cc.factorie.util.HyperparameterMain {
+  def evaluateParameters(args:Array[String]): Double = {
+
+    0.0
+  }
 }
 
 
-
-//object DocHeaderExtractor {
-//  def main(args:Array[String]): Unit = {
-//    println("DocHeaderExtractor")
-//  }
-//}
