21b284204ea993944f85aca469b3c4a7c4518c55
k8si <ksilvers@umass.edu>
2015-01-28 16:23:34 -0500

    typo

--- bin/train-header-tagger.sh
+++ bin/train-header-tagger.sh
@@ -5,4 +5,4 @@ MEMORY=20g
 data=$PH_ROOT/data/fullpaper-headers.tsv
 train="--train=$data"
 
-$PH_ROOT/bin/run_class.sh -Xmx $MEMORY edu.umass.cs.iesl.paperheader.crf.HeaderTaggerTrainer $train
+$PH_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.paperheader.crf.HeaderTaggerTrainer $train
