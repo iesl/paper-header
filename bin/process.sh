146b6d80a080432e6d823cbb6c69f1e18cefc028
k8si <ksilvers@umass.edu>
2015-02-11 13:38:46 -0500

    firstlast

--- bin/process.sh
+++ bin/process.sh
@@ -4,8 +4,9 @@ MEMORY=15g
 
 data="--file=$PH_ROOT/data/fullpaper-headers-modified.tsv"
 outfile="--out-file=$PH_ROOT/processed.tsv"
+fl="--firstlast=true"
 
 echo "processing $data ; output to $outfile"
 
-$PH_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.paperheader.process.DocProcessor $data $outfile
+$PH_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.paperheader.process.DocProcessor $data $outfile $fl
 
--- src/main/scala/edu/umass/cs/iesl/paperheader/process/ProcessDocs.scala
+++ src/main/scala/edu/umass/cs/iesl/paperheader/process/ProcessDocs.scala
@@ -18,6 +18,7 @@ class ProcessOpts extends CmdOptions{
   val dataFiles = new CmdOption("data-files", Nil.asInstanceOf[List[String]], "FILENAME...", "List of files to process (comma-separated).")
   val dataFile = new CmdOption("file", "", "STRING", "filename of data to process")
   val outFile = new CmdOption("out-file", "", "FILENAME...", "Output file.")
+  val extractFirstLastNames = new CmdOption("firstlast", false, "BOOLEAN", "extract first/last names as postprocessing step")
 }
 
 object DocProcessor {
@@ -41,8 +42,13 @@ object DocProcessor {
     println(s"processing ${opts.dataFile.value}...")
     docs.foreach(Pipeline.process1)
     if (opts.outFile.wasInvoked) {
-      println(s"writing to ${opts.outFile.value}...")
-      writeToTSV(opts.outFile.value, docs)
+      if (opts.extractFirstLastNames.value) {
+        firstLastNames(docs)
+//        writeToTSV(opts.outFile.value, docs, firstLast=true)
+      } else {
+        println(s"writing to ${opts.outFile.value}...")
+        writeToTSV(opts.outFile.value, docs)
+      }
     }
     println("done.")
   }
@@ -80,15 +86,21 @@ object DocProcessor {
    * @param outputFilename
    * @param docs
    */
-  def writeToTSV(outputFilename: String, docs: Seq[Document]): Unit = {
+  def writeToTSV(outputFilename: String, docs: Seq[Document], firstLast:Boolean = false): Unit = {
     import java.io._
     val pw = new PrintWriter(new File(outputFilename))
     docs.foreach(doc => {
       pw.write("--DOCSTART--\n")
       doc.sections.flatMap(_.tokens).foreach(token => {
         val column0 = token.string //token string
-        val column1 = token.attr[BioHeaderTag].categoryValue // tag
-        val column2 = token.attr[LabeledBioHeaderTag].target.categoryValue // gold label
+        val column1 = firstLast match {
+            case true => token.attr[BioHeaderTag2].categoryValue
+            case _ => token.attr[BioHeaderTag].categoryValue
+          }
+        val column2 = firstLast match {
+          case true => token.attr[LabeledBioHeaderTag2].categoryValue
+          case _ => token.attr[LabeledBioHeaderTag].categoryValue
+        }
         val column3 = if (column1 == column2) " " else "*"
         pw.write(s"$column0\t$column1\t$column2\t$column3\n")
       })
