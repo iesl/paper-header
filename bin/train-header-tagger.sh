9d1b6cda27d64bf53c95d5edb50433ad68c5a924
k8si <ksilvers@umass.edu>
2015-03-05 17:25:43 -0500

    fix

--- bin/train-header-tagger.sh
+++ bin/train-header-tagger.sh
@@ -3,13 +3,14 @@
 MEMORY=20g
 
 data="--data=data/fullpaper-headers.tsv"
+save="--serialize=true"
 
 l1=`$PH_ROOT/bin/get_config.sh l1`
 l2=`$PH_ROOT/bin/get_config.sh l2`
 rate=`$PH_ROOT/bin/get_config.sh rate`
 delta=`$PH_ROOT/bin/get_config.sh delta`
 
-$PH_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.paperheader.tagger.HeaderTaggerTrainer $data \
+$PH_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.paperheader.tagger.HeaderTaggerTrainer $data $save \
 "--l1=$l1" \
 "--l2=$l2" \
 "--learning-rate=$rate" \
