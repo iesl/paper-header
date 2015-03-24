3edebc09362801122d78c8f6e2ee481e67402310
k8si <ksilvers@cs.umass.edu>
2015-03-23 22:59:05 -0400

    fix typo

--- bin/optimize-header-tagger.sh
+++ bin/optimize-header-tagger.sh
@@ -7,4 +7,4 @@ CP="target/paper_header-0.1-SNAPSHOT-jar-with-dependencies.jar"
 
 #java -cp $CP "edu.umass.cs.iesl.paperheader.crf.HeaderTaggerTrainer" $train $dev
 
-$PH_ROOT/bin/run_class.sh "edu.umass.cs.iesl.paperheader.crf.HeaderTaggerTrainer" $train $dev
+$PH_ROOT/bin/run_class.sh "edu.umass.cs.iesl.paperheader.crf.HeaderTaggerOptimizer" $train $dev
--- bin/train-header-tagger.sh
+++ bin/train-header-tagger.sh
@@ -5,4 +5,4 @@ dev="--dev=$PH_ROOT/data/fullpaper-headers.dev"
 
 CP="target/paper_header-0.1-SNAPSHOT-jar-with-dependencies.jar"
 
-$PH_ROOT/bin/run_class.sh "edu.umass.cs.iesl.paperheader.crf.HeaderTaggerOptimizer" $train $dev
+$PH_ROOT/bin/run_class.sh "edu.umass.cs.iesl.paperheader.crf.HeaderTaggerTrainer" $train $dev
