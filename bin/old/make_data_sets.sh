33d3c48a77895befbda4535ca2c6715001cf786b
k8si <ksilvers@cs.umass.edu>
2015-05-23 14:26:31 -0400

    clean up a bunch of unused ephemera

--- bin/old/make_data_sets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-MEMORY=5g
-
-$PH_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.paperheader.utils.Utilities
--- bin/old/test-header-tagger.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MEMORY=15g
-
-model="--model=HeaderTagger.factorie"
-
-$PH_ROOT/bin/run_class.sh -Xmx$MEMORY edu.umass.cs.iesl.paperheader.tagger.HeaderTaggerTester $model
