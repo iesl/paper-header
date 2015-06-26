2bebfec61132ca1d02eba9b13547ad83574a773f
Emma Strubell <strubell@cs.umass.edu>
2015-06-26 10:11:37 -0400

    put good hyperparams in training script

--- bin/train-model.sh
+++ bin/train-model.sh
@@ -9,4 +9,8 @@ $PH_ROOT/bin/run_class.sh -Xmx${memSize} edu.umass.cs.iesl.paperheader.tagger.Tr
 --data-set="grobid" \
 --use-grobid-features="false" \
 --train-file="$PH_ROOT/grobid-header-train.data" \
---train-portion="1.0"
+--train-portion="1.0" \
+--learning-rate=0.3782168715643856 \
+--delta=0.0208848390678699 \
+--l2=0.15237729551609597 \
+--l1=0.05038601046083946
