ccfd35437993dd3d040d72e15026e8a968fdb47e
k8si <ksilvers@cs.umass.edu>
2015-11-14 17:24:53 -0500

    update scripts

---
+++ bin/train-default.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+taggerType="default"
+
+root="/home/kate/AI2/clean/paper-header"
+trainFile="$root/data/fullpaper-train.tsv"
+devFile="$root/data/fullpaper-dev.tsv"
+
+saveModel="true"
+modelFile="$root/HeaderTagger_default.factorie"
+
+mem="5G"
+facJar="/home/kate/research/factorie/target/factorie_2.11-1.2-SNAPSHOT-nlp-jar-with-dependencies.jar"
+CP="$root/target/paper_header-0.1-SNAPSHOT.jar:$facJar"
+
+java -Xmx$mem -cp $CP edu.umass.cs.iesl.paperheader.model.HeaderTaggerTrainer \
+--tagger-type=$taggerType \
+--train-file=$trainFile \
+--dev-file=$devFile \
+--save-model=$saveModel \
+--model-file=$modelFile
+
+
